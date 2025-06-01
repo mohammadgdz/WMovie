fb812c6f822d715cfc8d42cd414fc064dd3f6d22
mohammadgdz <mhd.gdz@gmail.com>
2025-06-01 23:05:27 +0330

    add pages

---
+++ WMovie/Coordinator/Pages.swift
@@ -0,0 +1,32 @@
+//
+//  Pages.swift
+//  WMovie
+//
+//  Created by Mohammad on 2025-06-01.
+//
+
+import Foundation
+
+enum Pages: String, Identifiable {
+    case login,
+         createAccount,
+         settings,
+         movies,
+         series,
+         actor,
+         movieDetails,
+         seriesDetails
+    
+    var id: String{
+        self.rawValue
+    }
+
+}
+
+enum Sheets: String, Identifiable {
+    case rating, review
+    
+    var id: String{
+        self.rawValue
+    }
+}
